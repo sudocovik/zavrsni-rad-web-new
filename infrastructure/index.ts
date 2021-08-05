94b5dfc5a1a90c35a9e3ea52ce3826370a098aae
covik <mate.nakic3@gmail.com>
2021-08-05 22:04:27 +0200

    Create basic K8S cluster

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,1 +1,15 @@
-import * as pulumi from "@pulumi/pulumi"
+import { KubernetesCluster, Region } from '@pulumi/digitalocean'
+
+const cluster: KubernetesCluster = new KubernetesCluster('main-cluster', {
+    name: 'main',
+    region: Region.FRA1,
+    nodePool: {
+        name: 'worker',
+        size: 's-1vcpu-2gb',
+        autoScale: false,
+        nodeCount: 1
+    },
+    version: '1.21.2-do.2',
+    autoUpgrade: false,
+    surgeUpgrade: false
+})
