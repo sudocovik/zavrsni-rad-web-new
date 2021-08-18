307131c16101ba75f93ea0ac349b7911e9858c54
covik <mate.nakic3@gmail.com>
2021-08-18 12:42:23 +0200

    Extract provisioning of cloud resources to module

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,47 +1,14 @@
-import {
-    Project,
-    Region,
-    Vpc,
-    KubernetesCluster
-} from '@pulumi/digitalocean'
 import { getStack, Output } from '@pulumi/pulumi'
+import { KubernetesCluster } from '@pulumi/digitalocean'
 import { Provider } from '@pulumi/kubernetes'
+import { provision as provisionCloudResources } from './src/cloud-resources'
 import { configure } from './k8s'
 import { readFileSync } from 'fs'
 
-const clusterName: string = 'final-thesis'
-
 if (getStack() === 'production') {
-    const region: Region = Region.FRA1
-
-    const vpc: Vpc = new Vpc('main-vpc', {
-        name: 'final-thesis',
-        region
-    })
-
-    const cluster: KubernetesCluster = new KubernetesCluster('main-cluster', {
-        name: clusterName,
-        region,
-        nodePool: {
-            name: 'worker',
-            size: 's-1vcpu-2gb',
-            autoScale: false,
-            nodeCount: 1
-        },
-        version: '1.21.2-do.2',
-        autoUpgrade: false,
-        surgeUpgrade: false,
-        vpcUuid: vpc.id
-    })
-
-    new Project('main-project', {
-        name: 'Final thesis',
-        description: 'Resources supporting final thesis',
-        purpose: 'Class project / Educational purposes',
-        resources: [ cluster.clusterUrn ]
-    })
-
+    const cluster: KubernetesCluster = provisionCloudResources()
     const kubeconfig: Output<string> = cluster.kubeConfigs[0].rawConfig
+
     const cloudKubernetesProvider: Provider = new Provider('cloud-provider', {
         kubeconfig
     })
---
+++ infrastructure/src/cloud-resources.ts
@@ -0,0 +1,35 @@
+import { KubernetesCluster, Project, Region, Vpc } from '@pulumi/digitalocean'
+
+export function provision() {
+    const region: Region = Region.FRA1
+    const clusterName: string = 'final-thesis'
+
+    const vpc: Vpc = new Vpc('main-vpc', {
+        name: 'final-thesis',
+        region
+    })
+
+    const cluster: KubernetesCluster = new KubernetesCluster('main-cluster', {
+        name: clusterName,
+        region,
+        nodePool: {
+            name: 'worker',
+            size: 's-1vcpu-2gb',
+            autoScale: false,
+            nodeCount: 1
+        },
+        version: '1.21.2-do.2',
+        autoUpgrade: false,
+        surgeUpgrade: false,
+        vpcUuid: vpc.id
+    })
+
+    new Project('main-project', {
+        name: 'Final thesis',
+        description: 'Resources supporting final thesis',
+        purpose: 'Class project / Educational purposes',
+        resources: [ cluster.clusterUrn ]
+    })
+
+    return cluster
+}
