543a9275d95de20bc73f214b7e9fc915d82a83fd
covik <mate.nakic3@gmail.com>
2021-08-23 12:53:22 +0200

    Abstract away provisioning of the backend and mysql

---
+++ infrastructure/src/kubernetes/backend/index.ts
@@ -0,0 +1,12 @@
+import { Provider } from '@pulumi/kubernetes'
+import * as k8s from '@pulumi/kubernetes'
+import provisionMysql from './mysql'
+import provisionApplication from './application'
+
+export default function (provider: Provider, dockerLogin: k8s.core.v1.Secret, isLocal: boolean): k8s.core.v1.Service {
+    const {
+        configuration: mysqlConfiguration,
+        service: mysqlService
+    } = provisionMysql(provider, isLocal)
+    return  provisionApplication(provider, mysqlConfiguration, mysqlService, dockerLogin, isLocal)
+}
--- infrastructure/src/kubernetes/index.ts
+++ infrastructure/src/kubernetes/index.ts
@@ -2,19 +2,16 @@ import { Provider } from '@pulumi/kubernetes'
 import * as k8s from '@pulumi/kubernetes'
 import provisionPersistentVolume from './local-volume'
 import provisionDockerLoginSecret from './docker-login'
-import provisionMysql from './backend/mysql'
-import provisionBackendApplication from './backend/application'
+import provisionBackendApplication from './backend'
 import provisionFrontendApplication from './frontend'
 import provisionIngress from './ingress'
 
 export default function(provider: Provider, isLocal: boolean = false) {
     isLocal && provisionPersistentVolume(provider)
     const dockerLogin: k8s.core.v1.Secret = provisionDockerLoginSecret(provider)
-    const {
-        configuration: mysqlConfiguration,
-        service: mysqlService
-    } = provisionMysql(provider, isLocal)
-    const backendApplication: k8s.core.v1.Service = provisionBackendApplication(provider, mysqlConfiguration, mysqlService, dockerLogin, isLocal)
+
+    const backendApplication: k8s.core.v1.Service = provisionBackendApplication(provider, dockerLogin, isLocal)
     const frontendApplication: k8s.core.v1.Service = provisionFrontendApplication(provider, dockerLogin)
+
     provisionIngress(provider, frontendApplication, backendApplication, isLocal)
 }
