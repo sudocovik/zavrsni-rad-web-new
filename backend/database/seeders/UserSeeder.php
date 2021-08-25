9f959c40ef5264bce25ad66a830e701554596611
covik <mate.nakic3@gmail.com>
2021-08-25 10:44:21 +0200

    Make seeder idempotent

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -14,9 +14,11 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'username' => 'master',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        ]);
+        if (User::query()->where('username', '=', 'master')->exists() === false) {
+            User::query()->create([
+                'username' => 'master',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            ]);
+        }
     }
 }
