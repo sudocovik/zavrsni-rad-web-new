691534bed3c50a1f353309849b9b71f59d9da10a
covik <mate.nakic3@gmail.com>
2021-08-28 13:59:46 +0200

    Rename method

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -2,17 +2,17 @@ import { getStack, Output } from '@pulumi/pulumi'
 import { KubernetesCluster } from '@pulumi/digitalocean'
 import { readFileSync } from 'fs'
 import provisionCloudResources from './src/cloud-resources'
-import configure from './src/kubernetes'
+import provisionKubernetesManifests from './src/kubernetes'
 
 if (getStack() === 'production') {
     const cluster: KubernetesCluster = provisionCloudResources()
     const kubeconfig: Output<string> = cluster.kubeConfigs[0].rawConfig
 
-    configure(kubeconfig)
+    provisionKubernetesManifests(kubeconfig)
 }
 
 if (getStack() == 'local') {
     const kubeconfig = readFileSync('./local/cluster/kubeconfig.yml', 'utf-8')
 
-    configure(kubeconfig, true)
+    provisionKubernetesManifests(kubeconfig, true)
 }
