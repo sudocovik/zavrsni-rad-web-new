ec9750f8c1e8354e982f93d95c12897cb65ca3e3
covik <mate.nakic3@gmail.com>
2021-08-29 11:49:30 +0200

    Change base url in local environment

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+process.env.NODE_ENV === 'development' && (axios.defaults.baseURL = 'http://localhost')
 
 new Vue({
   vuetify,
