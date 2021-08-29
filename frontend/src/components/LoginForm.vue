81391edb977a3e049b144662409885c90175da64
covik <mate.nakic3@gmail.com>
2021-08-29 11:42:58 +0200

    Add login view & form

---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,99 @@
+<template>
+    <v-form @submit.prevent="login">
+        <v-text-field
+            v-model="username.value"
+            :error-messages="username.errorMessages"
+
+            label="Korisničko ime"
+            prepend-inner-icon="mdi-account"
+            outlined
+        />
+
+        <v-text-field
+            v-model="password.value"
+            :error-messages="password.errorMessages"
+
+            type="password"
+            label="Lozinka"
+            prepend-inner-icon="mdi-lock"
+            outlined
+        />
+
+        <v-btn
+            color="primary"
+            type="submit"
+            block
+            outlined
+        >
+            Pošalji
+        </v-btn>
+    </v-form>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'LoginForm',
+
+  data: () => ({
+    username: {
+      value: '',
+      errorMessages: []
+    },
+    password: {
+      value: '',
+      errorMessages: []
+    },
+    loading: false
+  }),
+
+  methods: {
+    async login () {
+      this.loading = true
+      this.clearValidationErrorMessages()
+
+      try {
+        const { data } = await axios.post('/api/login', { username: this.username.value, password: this.password.value })
+        const serverResponse = data.data
+
+        this.loginSuccessful(serverResponse)
+      } catch (e) {
+        if (e.response.status === 422) {
+          const errors = e.response.data.errors
+          const {
+            username: usernameErrors = [],
+            password: passwordErrors = []
+          } = errors
+
+          this.username.errorMessages = usernameErrors
+          this.password.errorMessages = passwordErrors
+        } else if (e.response.status === 401) {
+          this.handleWrongCredentialsException()
+        } else if (e.response.status === 404) {
+          this.handleAlreadyLoggedInException()
+        }
+
+        this.loading = false
+      }
+    },
+
+    clearValidationErrorMessages () {
+      this.username.errorMessages = []
+      this.password.errorMessages = []
+    },
+
+    loginSuccessful (messageForUser) {
+      this.$emit('success', messageForUser)
+    },
+
+    handleWrongCredentialsException () {
+      this.$emit('error:wrong-credentials')
+    },
+
+    handleAlreadyLoggedInException () {
+      this.$emit('error:already-logged-in')
+    }
+  }
+}
+</script>
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,74 @@
+<template>
+    <v-app style="background-color: #eee;">
+        <v-container
+            fluid
+            class="fill-height"
+        >
+            <v-row
+                align="center"
+                justify="center"
+            >
+                <v-col
+                    cols="12"
+                    md="6"
+                    lg="3"
+                >
+                    <v-card elevation="6">
+                        <v-toolbar
+                            color="primary"
+                            flat
+                        >
+                            <span class="white--text text-h6">Prijava</span>
+                        </v-toolbar>
+
+                        <v-card-text>
+                            <LoginForm
+                                @success="redirectToDashboard"
+                                @error:wrong-credentials="notifyUserCredentialsAreWrong"
+                                @error:already-logged-in="redirectToDashboard"
+                            />
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+
+        <TheErrorMessage :message.sync="errorMessage" />
+    </v-app>
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm'
+import TheErrorMessage from '@/components/TheErrorMessage'
+
+export default {
+  name: 'LoginView',
+
+  components: {
+    LoginForm,
+    TheErrorMessage
+  },
+
+  data: () => ({
+    errorMessage: ''
+  }),
+
+  methods: {
+    showErrorMessage (message) {
+      this.errorMessage = message
+    },
+
+    redirectToDashboard () {
+      this.letParentHandleRedirection()
+    },
+
+    letParentHandleRedirection () {
+      this.$emit('logged-in')
+    },
+
+    notifyUserCredentialsAreWrong () {
+      this.showErrorMessage('Korisničko ime i/ili lozinka su ne ispravni.')
+    }
+  }
+}
+</script>
