a41584d98199ce302d319947e44467444c025ed7
covik <mate.nakic3@gmail.com>
2021-08-29 11:43:13 +0200

    Use login view temporarily

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <DashboardView />
+  <DashboardView v-if="false" />
+  <LoginView v-else />
 </template>
 
 <script>
 import DashboardView from '@/views/DashboardView'
+import LoginView from '@/views/LoginView'
 
 export default {
   name: 'App',
 
   components: {
+    LoginView,
     DashboardView
   }
 }
